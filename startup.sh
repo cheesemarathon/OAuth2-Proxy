d3d94b659d5439f92f22e429a83e38354cefd270
cheesemarathon <07bridgesb@gmail.com>
2018-03-01 16:34:15 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,11 +3,13 @@
 if [ -e /etc/oauth2/oauth2_proxy.cfg ]
 then
     wget https://raw.githubusercontent.com/cheesemarathon/oAuth2-Proxy-Docker-Container/master/oauth2_proxy.cfg -P /etc/oauth2/
+    chmod 664 /etc/oauth2/oauth2_proxy.cfg
 fi
 
 if [ -e /etc/oauth2/emails.cfg ]
 then
     wget https://raw.githubusercontent.com/cheesemarathon/oAuth2-Proxy-Docker-Container/master/emails.cfg -P /etc/oauth2/
+    chmod 664 /etc/oauth2/emails.cfg
 fi
 
 ./bin/oauth2_proxy -config=/etc/oauth2/oauth2_proxy.cfg
