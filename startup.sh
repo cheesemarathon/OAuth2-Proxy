a31b043b90460792b7479a3703735fa51f3eea53
cheesemarathon <07bridgesb@gmail.com>
2018-03-01 16:07:15 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 wget https://raw.githubusercontent.com/cheesemarathon/oAuth2-Proxy-Docker-Container/master/oauth2_proxy.cfg -P /etc/oauth2/
 wget https://raw.githubusercontent.com/cheesemarathon/oAuth2-Proxy-Docker-Container/master/emails.cfg -P /etc/oauth2/
 
-./bin/oauth2_proxy --config=/etc/oauth2/oauth2_proxy.cfg
+./bin/oauth2_proxy -config=/etc/oauth2/oauth2_proxy.cfg
