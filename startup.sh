4ab0705753cc642f42385baf0f8eb8d95e16fc03
cheesemarathon <07bridgesb@gmail.com>
2018-03-02 14:38:36 +0000

    Fix startup script file check

--- startup.sh
+++ startup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ -e /etc/oauth2/oauth2_proxy.cfg ]
+file="/etc/oauth2/oauth2_proxy.cfg"
+if [ -f "$file" ]
 then
+	echo "$file found."
+else
+	echo "$file not found."
     wget https://raw.githubusercontent.com/cheesemarathon/OAuth2-Proxy/master/oauth2_proxy.cfg -P /etc/oauth2/
     chmod 664 /etc/oauth2/oauth2_proxy.cfg
 fi
 
-if [ -e /etc/oauth2/emails.cfg ]
+file=/etc/oauth2/emails.cfg
+if [ -f "$file" ]
 then
+	echo "$file found."
+else
+	echo "$file not found."
     wget https://raw.githubusercontent.com/cheesemarathon/OAuth2-Proxy/master/emails.cfg -P /etc/oauth2/
     chmod 664 /etc/oauth2/emails.cfg
 fi
