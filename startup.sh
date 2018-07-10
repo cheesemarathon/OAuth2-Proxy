84e8f3e0c1449c5cd4f43dd0f970547c47e53e3a
Ben <07bridgesb@gmail.com>
2018-07-10 10:57:02 +0000

    Add templates

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ else
     chmod 664 /etc/oauth2/oauth2_proxy.cfg
 fi
 
-file=/etc/oauth2/emails.cfg
+file="/etc/oauth2/emails.cfg"
 if [ -f "$file" ]
 then
 	echo "$file found."
@@ -18,6 +18,26 @@ else
 	echo "$file not found."
     wget https://gitlab.squishedmooo.com/cheesemarathon/OAuth2-Proxy/raw/master/startup.sh -P /etc/oauth2/
     chmod 664 /etc/oauth2/emails.cfg
+fi
+
+file="/etc/oauth2/templates/sign_in.html"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+    wget https://gitlab.squishedmooo.com/cheesemarathon/OAuth2-Proxy/raw/master/templates/sign_in.html -P /etc/oauth2/templates/
+    chmod 664 /etc/oauth2/templates/sign_in.html
+fi
+
+file="/etc/oauth2/templates/error.html"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+    wget https://gitlab.squishedmooo.com/cheesemarathon/OAuth2-Proxy/raw/master/templates/error.html -P /etc/oauth2/templates/
+    chmod 664 /etc/oauth2/templates/error.html
 fi
 
 exec /bin/oauth2_proxy -config=/etc/oauth2/oauth2_proxy.cfg
